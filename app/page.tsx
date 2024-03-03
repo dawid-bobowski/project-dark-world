0ab56b685fe4d279f200d157bc3835f73247aa21
Dawid Bobowski <dbobowski3@gmail.com>
2024-03-03 23:58:11 +0100

    add loading page

--- app/page.tsx
+++ app/page.tsx
@@ -14,16 +14,16 @@ const HomePage: React.FC = () => {
 
   return <Loading />;
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <Link
-        href="/login"
-        className={`${unifrakturCook.className} flex justify-center align-center text-4xl text-center text-[var(--pale-dogwood)] p-5`}
-      >
-        <p>{">> Click here to enter the Dark World <<"}</p>
-      </Link>
-    </main>
-  );
+  // return (
+  //   <main className="flex min-h-screen flex-col items-center justify-center">
+  //     <Link
+  //       href="/login"
+  //       className={`${unifrakturCook.className} flex justify-center align-center text-4xl text-center text-[var(--pale-dogwood)] p-5`}
+  //     >
+  //       <p>{">> Click here to enter the Dark World <<"}</p>
+  //     </Link>
+  //   </main>
+  // );
 };
 
 export default HomePage;
---
+++ app/sign-a-pact/loading.tsx
@@ -0,0 +1,7 @@
+import Loading from "@/components/common/Loading";
+
+const LoginLoading: React.FC = () => {
+  return <Loading />;
+};
+
+export default LoginLoading;
